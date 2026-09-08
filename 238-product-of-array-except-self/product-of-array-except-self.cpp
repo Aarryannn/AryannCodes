784ae0a40612df7e139f0d098739a9cd61f1b1a8
ARRYANNN <arryann023@gmail.com>
2026-09-08 20:06:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (41.19%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -4,19 +4,21 @@ public:
         int n = nums.size();
         // if(n == 0) return {};
         vector<int> left(n);
-        vector<int> right(n);
+        // vector<int> right(n);
 
         left[0] = 1;
-        right[n-1] = 1;
+        // right[n-1] = 1;
 
         for(int i = 1; i < n; i++){
             left[i] = left[i-1] * nums[i-1];
-            right[n-i-1] = right[n-i] * nums[n-i];
+            // right[n-i-1] = right[n-i] * nums[n-i];
         }
     vector<int> ans(n);
-
-        for(int i = 0; i < n; i++){
-            ans[i] = left[i] * right[i];
+        int r = 1;
+        for(int i = n-1; i >= 0 ; i--){
+            ans[i] = left[i]  * r;
+            r = nums[i]* r;
+            
         }
 
         return ans;
