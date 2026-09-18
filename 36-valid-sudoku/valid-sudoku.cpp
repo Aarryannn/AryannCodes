b500e21f4ead1c1f66208e8704e9b03c698d5c20
ARRYANNN <arryann023@gmail.com>
2026-09-18 12:21:01 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (53.91%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& nums) {
-       
-       int row[9][9] = {0};
-       int col[9][9] = {0};
-       int box[9][9] = {0};
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+        int row[9][9];
+        int col[9][9];
+        int box[9][9];
 
-       for(int i = 0 ; i < 9; i++){
-        for(int j = 0; j < 9; j++){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
 
-            if(nums[i][j] == '.') continue;
+                if(board[i][j] == '.') continue;
 
-            int val = nums[i][j] - '1';
-            int box_num = (i/3)*3 + (j/3);
+                int val = board[i][j] - '0'-1;
+                int idx = (i/3)*3 + (j/3);
 
+                if(row[i][val] == 1 || col[j][val] == 1 || box[idx][val] == 1 ) return false;
 
-            if(row[i][val] == 1 || col[j][val] == 1 || box[box_num][val] == 1) return false;
-
-            row[i][val] = 1;
-            col[j][val] = 1;
-            box[box_num][val] = 1;
-
+                row[i][val] = 1;
+                col[j][val] = 1;
+                box[idx][val] = 1;
+            }
         }
-       }
-
-       return true;
-
+        return true;
     }
 };
