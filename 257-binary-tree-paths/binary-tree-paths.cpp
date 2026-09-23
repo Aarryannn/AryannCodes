dee4e8040f35f2621671ca339dd1eb2c5ccff04a
ARRYANNN <arryann023@gmail.com>
2026-09-23 11:05:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (35.18%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(TreeNode* root, string path, vector<string>& ans) {
+        if (!root)
+            return;
+
+        path += to_string(root->val);
+
+        if (!root->left && !root->right) {
+            ans.push_back(path);
+            return;
+        }
+
+        path += "->";
+
+        solve(root->left, path, ans);
+        solve(root->right, path, ans);
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        solve(root, "", ans);
+        return ans;
+    }
+};
